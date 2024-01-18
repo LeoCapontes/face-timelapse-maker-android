e579ae425b944629bbe2102aa70563206ae0270e
LeoCapontes <44732043+LeoCapontes@users.noreply.github.com>
2024-01-18 20:49:47 +0000

    Created factory for result screen viewmodel

--- app/src/main/java/com/jlpc/facetimelapsemaker/viewmodel/ResultViewModel.kt
+++ app/src/main/java/com/jlpc/facetimelapsemaker/viewmodel/ResultViewModel.kt
@@ -1,9 +1,12 @@
 package com.jlpc.facetimelapsemaker.viewmodel
 
 import android.content.Context
+import android.net.Uri
 import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.jlpc.facetimelapsemaker.FaceTimelapseMakerApp
@@ -12,9 +15,9 @@ import com.jlpc.facetimelapsemaker.model.PreferenceManager
 import com.jlpc.facetimelapsemaker.utils.Encoder
 import com.jlpc.facetimelapsemaker.utils.createTimelapse
 import com.jlpc.facetimelapsemaker.utils.saveImagesToCache
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
+// TODO add factory
 class ResultViewModel(
     private val repository: PhotoRepository = FaceTimelapseMakerApp.repository,
     private val appContext: Context,
@@ -22,6 +25,10 @@ class ResultViewModel(
     val TAG: String = "ResultViewModel"
     val preferenceManager: PreferenceManager = FaceTimelapseMakerApp.preferences
     val fpsLiveData: LiveData<Int?> = preferenceManager.fpsFlow.asLiveData()
+    val uriLiveData: MutableLiveData<Uri> by lazy {
+        MutableLiveData<Uri>(Uri.parse("${appContext.cacheDir}/test.mp4"))
+    }
+    val timelapseGenerated: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>(false) }
 
     fun launchTimelapseCommand() {
         viewModelScope.launch {
@@ -33,12 +40,27 @@ class ResultViewModel(
                     // TODO change placeholder
                     if (fps != 0) {
                         createTimelapse(fps, appContext.cacheDir.path, Encoder.MP4, "1920x1080")
+                        Log.d(TAG, "timelapse complete")
+                        timelapseGenerated.value = true
                     } else {
                         Log.d(TAG, "fps is null")
                     }
                 }
             }
-            // TODO clear cache after timelapse generated
+            // TODO clear cached images after timelapse generated
         }
+    }
+}
+
+class ResultViewModelFactory(
+    private val repository: PhotoRepository,
+    private val appContext: Context
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ResultViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ResultViewModel(repository, appContext) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
