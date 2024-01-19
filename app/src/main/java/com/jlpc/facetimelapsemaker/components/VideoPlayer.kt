21bcc05d3af024faa16f6881273989de67d70fc7
LeoCapontes <44732043+LeoCapontes@users.noreply.github.com>
2024-01-19 17:55:48 +0000

    Video player now has media controller

--- app/src/main/java/com/jlpc/facetimelapsemaker/components/VideoPlayer.kt
+++ app/src/main/java/com/jlpc/facetimelapsemaker/components/VideoPlayer.kt
@@ -2,11 +2,13 @@ package com.jlpc.facetimelapsemaker.components
 
 import android.net.Uri
 import android.view.LayoutInflater
+import android.widget.MediaController
 import android.widget.VideoView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,12 +17,14 @@ import com.jlpc.facetimelapsemaker.R
 
 @Composable
 fun VideoPlayer(modifier: Modifier = Modifier, uri: Uri) {
+    val context = LocalContext.current
+    val mediaController = remember {MediaController(context)}
     AndroidView(
         factory = { context ->
             val view = LayoutInflater.from(context)
                 .inflate(R.layout.video_view_layout, null, false) as VideoView
             view.setVideoURI(uri)
-            view.start()
+            view.setMediaController(mediaController)
             view
         },
         update = {},
@@ -32,5 +36,5 @@ fun VideoPlayer(modifier: Modifier = Modifier, uri: Uri) {
 fun PreviewVideoPlayer() {
     val uri = Uri.parse("${LocalContext.current.cacheDir}/test.mp4")
     val modifier = Modifier.fillMaxSize()
-    VideoPlayer(modifier = modifier, uri = uri)
+    //VideoPlayer(modifier = modifier, uri = uri)
 }
