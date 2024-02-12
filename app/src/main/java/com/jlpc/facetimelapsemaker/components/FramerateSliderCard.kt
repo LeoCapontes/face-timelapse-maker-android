50c255bf72e0357e54531ec305de9fd030e777c0
LeoCapontes <44732043+LeoCapontes@users.noreply.github.com>
2024-02-12 19:34:03 +0000

    Video config slider now reflects saved fps preference. Fixes issue of slider position defaulting to 1 when saved value to be used in timelapse was something else

--- app/src/main/java/com/jlpc/facetimelapsemaker/components/FramerateSliderCard.kt
+++ app/src/main/java/com/jlpc/facetimelapsemaker/components/FramerateSliderCard.kt
@@ -4,23 +4,32 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.tooling.preview.Preview
 import com.jlpc.facetimelapsemaker.viewmodel.VideoConfigViewModel
+import kotlinx.coroutines.flow.map
 
 @Composable
-fun FramerateSliderCard(maxFramerate: Float = 24f, viewModel: VideoConfigViewModel) {
+fun FramerateSliderCard(
+    maxFramerate: Float = 24f,
+    viewModel: VideoConfigViewModel,
+) {
     var sliderPosition by remember { mutableFloatStateOf(1f) }
+    val fpsPreference: Float =
+        viewModel.preferenceManager.fpsFlow
+            .map { it?.toFloat() ?: 1f }
+            .collectAsState(initial = 1f).value
+    sliderPosition = fpsPreference
     Column {
         Slider(
             value = sliderPosition,
             onValueChange = {
                 sliderPosition = it
                 viewModel.saveSelectedFPS(it.toInt())
-                },
+            },
             steps = maxFramerate.toInt() - 1,
             valueRange = 1f..maxFramerate,
         )
