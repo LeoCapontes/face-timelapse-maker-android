fd6190f1d679e4633b42c7b1f1dcb4770e70aa9c
LeoCapontes <44732043+LeoCapontes@users.noreply.github.com>
2024-05-13 02:05:01 +0100

    Improve composable previews.

--- app/src/main/java/com/jlpc/facetimelapsemaker/components/ImportedPhotoGrid.kt
+++ app/src/main/java/com/jlpc/facetimelapsemaker/components/ImportedPhotoGrid.kt
@@ -1,39 +1,34 @@
 package com.jlpc.facetimelapsemaker.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.jlpc.facetimelapsemaker.mocks.mockUIModelList
+import com.jlpc.facetimelapsemaker.mocks.mockPhotoEntityList
 import com.jlpc.facetimelapsemaker.model.PhotoEntity
-import com.jlpc.facetimelapsemaker.model.PhotoUiModel
 
 @Composable
-fun ImportedPhotoGrid(uiModelList: List<PhotoEntity>) {
+fun ImportedPhotoGrid(photoEntityList: List<PhotoEntity>) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(124.dp),
         content = {
-            items(uiModelList.size) {
+            items(photoEntityList.size) {
                     index ->
-                PhotoContainer(uiModelList[index])
+                PhotoContainer(photoEntityList[index])
             }
         },
     )
 }
 
-@Composable
 @Preview
-fun PreviewImportedPhotoGrid() {
-    val testPhotoList: List<PhotoUiModel> = mockUIModelList
-
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(120.dp),
-        content = {
-            items(testPhotoList.size) {
-                    index ->
-                OLDPhotoContainer(testPhotoList[index])
-            }
-        },
-    )
+@Preview(name = "NEXUS_7", device = Devices.NEXUS_7)
+@Preview(name = "PIXEL_4", device = Devices.PIXEL_4)
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview()
+@Composable
+fun PreviewPhotoGrid() {
+    ImportedPhotoGrid(photoEntityList = mockPhotoEntityList)
 }
