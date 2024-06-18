976d8f8c606bc7f4b118645d8102c7036315e166
LeoCapontes <leo8capontes@hotmail.co.uk>
2024-06-18 18:55:20 +0100

    Animate image expansion.

--- app/src/main/java/com/jlpc/facetimelapsemaker/components/ExpandedPhotoContainer.kt
+++ app/src/main/java/com/jlpc/facetimelapsemaker/components/ExpandedPhotoContainer.kt
@@ -1,17 +1,20 @@
 package com.jlpc.facetimelapsemaker.components
 
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -20,25 +23,26 @@ import com.jlpc.facetimelapsemaker.model.PhotoEntity
 @Composable
 fun ExpandedPhotoContainer(
     entity: PhotoEntity,
-
 ) {
+    var scale: Float by remember { mutableFloatStateOf(0.2f) }
     Box(
         contentAlignment = Alignment.Center,
-        modifier =
-        Modifier
+
+        modifier = Modifier
             .padding(64.dp)
-            .fillMaxWidth()
+            .fillMaxSize(scale)
             .aspectRatio(1f)
             .clip(shape = MaterialTheme.shapes.large)
-            .shadow(4.dp)
-            .pointerInput(Unit) {
-                detectTapGestures()
-            },
+            .animateContentSize()
     ) {
         AsyncImage(
             model = entity.uri,
             contentDescription = null,
             contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
+    }
+    LaunchedEffect(true) {
+        scale = 1f
     }
 }
