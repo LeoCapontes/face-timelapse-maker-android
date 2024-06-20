53d9102125e0f90091e4a0782421c28ae004d6bf
LeoCapontes <44732043+LeoCapontes@users.noreply.github.com>
2024-06-20 19:15:10 +0100

    Remove redundant onExpandImage function wrapper.

--- app/src/main/java/com/jlpc/facetimelapsemaker/components/ImportedPhotoGrid.kt
+++ app/src/main/java/com/jlpc/facetimelapsemaker/components/ImportedPhotoGrid.kt
@@ -1,7 +1,6 @@
 package com.jlpc.facetimelapsemaker.components
 
 import android.content.res.Configuration
-import android.net.Uri
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -9,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jlpc.facetimelapsemaker.mocks.mockPhotoEntityList
 import com.jlpc.facetimelapsemaker.model.PhotoEntity
 
 @Composable
@@ -16,16 +16,12 @@ fun ImportedPhotoGrid(
     photoEntityList: List<PhotoEntity>,
     onExpandImage: (PhotoEntity) -> Unit,
 ) {
-    val onImageExpand: (PhotoEntity) -> Unit = { entity ->
-        onExpandImage(entity)
-    }
 
     LazyVerticalGrid(
         columns = GridCells.Adaptive(124.dp),
         content = {
-            items(photoEntityList) {
-                    photo ->
-                PhotoContainer(photo, onImageExpand)
+            items(photoEntityList) { photo ->
+                PhotoContainer(photo, onExpandImage)
             }
         },
     )
@@ -34,9 +30,12 @@ fun ImportedPhotoGrid(
 @Preview
 @Preview(name = "NEXUS_7", device = Devices.NEXUS_7)
 @Preview(name = "PIXEL_4", device = Devices.PIXEL_4)
+@Preview(name = "PIXEL_3", device = Devices.PIXEL_3)
 @Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Preview()
 @Composable
 fun PreviewPhotoGrid() {
-    // ImportedPhotoGrid(photoEntityList = mockPhotoEntityList)
+    val onImageExpand: (PhotoEntity) -> Unit = { entity ->}
+
+    ImportedPhotoGrid(photoEntityList = mockPhotoEntityList, onImageExpand)
 }
